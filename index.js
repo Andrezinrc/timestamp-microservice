617df018a4b1385e3335ca804475a1334b8c5d98
Andrezinrc <andrem.33333@gmail.com>
2024-09-11 18:22:12 -0300

    Primeiro commit do projeto de Microserviço de Carimbo de Data/Hora

--- index.js
+++ index.js
@@ -18,12 +18,24 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", function (req, res) {
+  let dateParam = req.params.date;
+  
+  let date = dateParam ? 
+      (isNaN(dateParam) ? new Date(dateParam) : new Date(parseInt(dateParam))) 
+      : new Date();
+  
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Data invalida" });
+  }
+  
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
 
 
 // Listen on port set in environment variable or default to 3000
